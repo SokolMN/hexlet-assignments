7dcf66da4316b2ef91aa9f171972ed069b247650
@hexlet/cli <support@hexlet.io>
2024-01-21 20:00:42 +0300

    submit java-spring-ru/response-entity

---
+++ java-spring-ru/response-entity/src/main/java/exercise/model/Post.java
@@ -0,0 +1,42 @@
+package exercise.model;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+public class Post {
+    private String id;
+    private String title;
+    private String body;
+
+    public String getBody() {
+        return body;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+
+}
